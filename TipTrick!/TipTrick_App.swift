9a7c38d039cb7f6d0586b9b4d04357856d46626e
heflinbh <heflinbh@miamioh.edu>
2022-10-27 14:12:06 -0400

    Initial Commit

---
+++ TipTrick!/TipTrick_App.swift
@@ -0,0 +1,17 @@
+//
+//  TipTrick_App.swift
+//  TipTrick!
+//
+//  Created by Benjamin Heflin on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TipTrick_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
