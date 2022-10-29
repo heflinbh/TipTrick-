613b7526d5216d69e2436a6c1371c07a5bcb6a2c
heflinbh <heflinbh@miamioh.edu>
2022-10-29 15:11:06 -0400

    Zero tip not gives red text

--- TipTrick!/ContentView.swift
+++ TipTrick!/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numPeople = 2
+    @State private var tipPercent = 20
+    
+    @State private var currentCurrencyCode = FloatingPointFormatStyle<Double>.Currency.init(code: Locale.current.currencyCode ?? "USD")
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentChoices = [10, 15, 20, 25, 0]
+    
+    var getGrandTotal: Double {
+        let tipSelection = Double(tipPercent)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        getGrandTotal / Double(numPeople + 2)
+    }
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of People", selection: $numPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.automatic)
+                }
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercent) {
+                        ForEach(tipPercentChoices, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                header: {
+                    Text("How much tip do you want to leave?")
+                }
+                Section {
+                    Text(getGrandTotal, format: currentCurrencyCode)
+                }
+                header : {
+                    Text("Total amount for check:")
+                }
+                Section {
+                    Text(totalPerPerson, format: currentCurrencyCode)
+                        .padding()
+                        .foregroundColor(tipPercent == 0 ? .red : .primary)
+                }
+                header : {
+                    Text("Amount per person:")
+                }
+            }
+            .navigationTitle("TipTrick!")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
